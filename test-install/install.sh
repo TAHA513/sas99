8f290bf8b466741ddb6d461db26067d3066a70d5
asaad11asaad98 <39617731-asaad11asaad98@users.noreply.replit.com>
2025-02-17 16:45:09 +0000

    Restored to '831e9e5a59a63902bc6775e0dddaec00f0bea894'
    Replit-Restored-To:831e9e5a59a63902bc6775e0dddaec00f0bea894

--- test-install/install.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/bash
-echo "Installing Digital Business Manager..."
-npm install
-echo "Creating desktop shortcut..."
-bash create-shortcut.sh
-echo "Installation completed! You can now start the application from your desktop."
-  
